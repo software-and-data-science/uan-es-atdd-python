0ec151856232307f0631e2af8aa0f3bc6787f96c
Alejandro Sánchez <jasanchez@unal.edu.co>
2020-04-28 15:40:50 -0500

    Se corrigen pruebas

--- calculator.py
+++ calculator.py
@@ -1,8 +1,6 @@
 class Calculator():
-    """docstring for Calculadora."""
-
     def add(self, value1, value2):
         return value1 + value2
 
-    def restar(self, value1, value2):
+    def subtract(self, value1, value2):
         return value1 - value2
